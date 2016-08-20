ab8c6c4e77bcb94a1e1e418717ec39cdf16304a0
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-08-20 18:31:02 -0400

    adding about page

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -25,4 +25,18 @@ feature "Static pages" do
 		end 
 	end
 
+	feature "Help page" do
+		it "should have the content 'help'" do
+			visit 'static_pages/help'
+			page.should have_content('Help')
+		end
+	end
+
+	feature "About page" do
+		it "should have the content 'About Us'" do
+			visit 'static_pages/about'
+			page.should have_content('About Us')
+		end
+	end
+
 end
