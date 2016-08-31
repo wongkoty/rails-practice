f5146c4ee1d72e587217dc931a3a3a11be73e0ae
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-08-31 14:27:42 -0400

    adding ssl

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+SampleApp::Application.configure do
+	# Force all access to the app over SSL, use Strict-Transport-Security, # and use secure cookies.
+	config.force ssl = true
+end
